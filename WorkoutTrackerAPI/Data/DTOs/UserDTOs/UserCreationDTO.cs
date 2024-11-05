25396e749d00063ae1341341624c1e9de813cc63
Kurulko <kurul300013@gmail.com>
2024-11-05 08:42:56 +0200

    Added new properties to User DTOs

--- WorkoutTrackerAPI/Data/DTOs/UserDTOs/UserCreationDTO.cs
+++ WorkoutTrackerAPI/Data/DTOs/UserDTOs/UserCreationDTO.cs
@@ -1,4 +1,5 @@
 ﻿using WorkoutTrackerAPI.Data.Models;
+using WorkoutTrackerAPI.Data.Models.UserModels;
 using WorkoutTrackerAPI.ValidationAttributes;
 
 namespace WorkoutTrackerAPI.Data.DTOs;
@@ -7,6 +8,9 @@ public class UserCreationDTO : IEquatable<User>
 {
     public string UserName { get; set; } = null!;
     public string? Email { get; set; }
+
+    public WeightType PreferableWeightType { get; set; }
+    public SizeType PreferableSizeType { get; set; }
 
     [DateNotInFuture]
     public DateTime? StartedWorkingOut { get; set; }
