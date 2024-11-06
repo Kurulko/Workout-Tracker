194f9959e9a753a6b5bd9a7bf28ecee211749333
Kurulko <kurul300013@gmail.com>
2024-11-06 14:56:37 +0200

    Imrpoved ExerciseRepository

--- WorkoutTrackerAPI/Repositories/WorkoutRepositories/ExerciseRepository.cs
+++ WorkoutTrackerAPI/Repositories/WorkoutRepositories/ExerciseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using WorkoutTrackerAPI.Data.Models.UserModels;
 using WorkoutTrackerAPI.Data;
 using WorkoutTrackerAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WorkoutTrackerAPI.Repositories;
 
@@ -9,5 +10,27 @@ public class ExerciseRepository : BaseWorkoutRepository<Exercise>
     public ExerciseRepository(WorkoutDbContext db) : base(db)
     {
 
+    }
+
+    IQueryable<Exercise> GetExercises()
+        => dbSet.Include(m => m.WorkingMuscles);
+
+    public override Task<IQueryable<Exercise>> GetAllAsync()
+        => Task.FromResult(GetExercises());
+
+    public override async Task<Exercise> AddAsync(Exercise model)
+    {
+        if (model.WorkingMuscles is not null)
+        {
+            var muscleIds = model.WorkingMuscles.Select(c => c.Id).ToList();
+
+            var existingMuscles = await db.Muscles
+                .Where(m => muscleIds.Contains(m.Id))
+                .ToListAsync();
+
+            model.WorkingMuscles = existingMuscles;
+        }
+
+        return await base.AddAsync(model);
     }
 }
