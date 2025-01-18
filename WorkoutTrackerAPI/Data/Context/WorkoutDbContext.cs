5dc607164faf5197357eb5aa312a86d93b823b53
Kurulko <kurul300013@gmail.com>
2025-01-18 14:03:12 +0200

    Updated WorkoutDbContext

--- WorkoutTrackerAPI/Data/Context/WorkoutDbContext.cs
+++ WorkoutTrackerAPI/Data/Context/WorkoutDbContext.cs
@@ -1,23 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.Metrics;
+using WorkoutTrackerAPI.Data.DTOs.WorkoutDTOs;
 using WorkoutTrackerAPI.Data.Models;
 using WorkoutTrackerAPI.Data.Models.UserModels;
 using WorkoutTrackerAPI.Data.Models.WorkoutModels;
-using WorkoutTrackerAPI.Exceptions;
+using WorkoutTrackerAPI.Extentions;
 
 namespace WorkoutTrackerAPI.Data;
 
 public class WorkoutDbContext : IdentityDbContext<User>
 {
+    public DbSet<UserDetails> UsersDetails => Set<UserDetails>();
     public DbSet<Exercise> Exercises => Set<Exercise>();
     public DbSet<Muscle> Muscles => Set<Muscle>();
-    public DbSet<Workout> Workouts => Set<Workout>();
     public DbSet<MuscleSize> MuscleSizes => Set<MuscleSize>();
     public DbSet<BodyWeight> BodyWeights => Set<BodyWeight>();
-    public DbSet<ExerciseRecord> ExerciseRecords => Set<ExerciseRecord>();
     public DbSet<Equipment> Equipments => Set<Equipment>();
+    public DbSet<ExerciseRecord> ExerciseRecords => Set<ExerciseRecord>();
+    public DbSet<ExerciseRecordGroup> ExerciseRecordGroups => Set<ExerciseRecordGroup>();
+    public DbSet<ExerciseSet> ExerciseSets => Set<ExerciseSet>();
+    public DbSet<ExerciseSetGroup> ExerciseSetGroups => Set<ExerciseSetGroup>();
+    public DbSet<WorkoutRecord> WorkoutRecords => Set<WorkoutRecord>();
+    public DbSet<Workout> Workouts => Set<Workout>();
 
     public WorkoutDbContext(DbContextOptions options) : base(options)
          => Database.EnsureCreated();
@@ -25,6 +29,31 @@ public class WorkoutDbContext : IdentityDbContext<User>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyModelWeightConversion<BodyWeight>(bw => bw.Weight);
+        modelBuilder.ApplyModelSizeConversion<MuscleSize>(ms => ms.Size);
+        modelBuilder.ApplyModelWeightConversion<UserDetails>(ud => ud.Weight);
+        modelBuilder.ApplyModelSizeConversion<UserDetails>(ud => ud.Height);
+        modelBuilder.ApplyModelWeightConversion<ExerciseSet>(m => m.Weight);
+        modelBuilder.ApplyModelWeightConversion<ExerciseRecord>(m => m.Weight);
+
+        modelBuilder.Entity<ExerciseSetGroup>()
+            .HasMany(w => w.ExerciseSets)
+            .WithOne(u => u.ExerciseSetGroup)
+            .HasForeignKey(e => e.ExerciseSetGroupId)
+            .OnDelete(DeleteBehavior.ClientCascade);
+
+        modelBuilder.Entity<ExerciseRecordGroup>()
+            .HasMany(w => w.ExerciseRecords)
+            .WithOne(u => u.ExerciseRecordGroup)
+            .HasForeignKey(e => e.ExerciseRecordGroupId)
+            .OnDelete(DeleteBehavior.ClientCascade);
+
+        modelBuilder.Entity<Workout>()
+            .HasMany(w => w.WorkoutRecords)
+            .WithOne(u => u.Workout)
+            .HasForeignKey(e => e.WorkoutId)
+            .OnDelete(DeleteBehavior.ClientCascade);
 
         modelBuilder.Entity<Exercise>()
             .HasOne(e => e.CreatedByUser)
@@ -49,3 +78,4 @@ public class WorkoutDbContext : IdentityDbContext<User>
             .HasForeignKey(er => er.ExerciseId);
     }
 }
+
