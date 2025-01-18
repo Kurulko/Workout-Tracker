1ce4e890969bfc297b9fb33f8dfac1e57a25d744
Kurulko <kurul300013@gmail.com>
2025-01-18 14:07:22 +0200

    Updated Program.cs

--- WorkoutTrackerAPI/Program.cs
+++ WorkoutTrackerAPI/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.FileProviders;
 using WorkoutTrackerAPI.Providers;
+using WorkoutTrackerAPI.Services.FileServices;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -7,6 +9,9 @@ var services = builder.Services;
 
 services.AddControllersWithOptions();
 
+services.AddDistributedMemoryCache();
+services.AddSession();
+
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
@@ -14,6 +19,7 @@ services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 services.AddMSSQLServer(config);
 
+services.AddTransient<IFileService, FileService>();
 services.AddIdentityModels();
 
 services.AddJWTAuthentication(config);
@@ -32,12 +38,20 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Data/Source")),
+    RequestPath = "/Resources"
+});
 
 await app.InitializeDataAsync(config);
 
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseSession();
 app.MapControllers();
 
 app.Run();
