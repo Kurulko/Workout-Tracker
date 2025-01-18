8f20f85780b58a25fd800efbbf1921c2f2ffb018
Kurulko <kurul300013@gmail.com>
2025-01-18 13:32:06 +0200

    Added editor components

---
+++ WorkoutTracker/src/app/shared/components/editors/exercise-set-groups-editor/exercise-set-groups-editor.component.ts
@@ -0,0 +1,154 @@
+import { Component, EventEmitter, Input, Output, ViewChild, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { ExerciseSetGroup } from '../../../models/exercise-set-group';
+import { ModelWeight } from '../../../models/model-weight';
+import { ExerciseService } from 'src/app/exercises/services/exercise.service';
+import { BaseEditorComponent } from '../../base-editor.component';
+import { WeightType } from 'src/app/shared/models/weight-type';
+import { MatAccordion } from '@angular/material/expansion';
+import { showCountOfSomethingStr } from 'src/app/shared/helpers/functions/showFunctions/showCountOfSomethingStr';
+import { Exercise } from 'src/app/exercises/models/exercise';
+
+@Component({
+  selector: 'app-exercise-set-groups-editor',
+  templateUrl: './exercise-set-groups-editor.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ExerciseSetGroupsEditorComponent),
+      multi: true,
+    },
+  ],
+})
+export class ExerciseSetGroupsEditorComponent extends BaseEditorComponent<ExerciseSetGroup[]>{
+  @Input() weightTypeValue?: WeightType;
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
+  
+  private isValid: boolean = false;
+  @Output() validityChange = new EventEmitter<boolean>();
+  
+  exerciseSetGroups!: ExerciseSetGroup[];
+  private exerciseSetGroupsValidators!: boolean[];
+
+  constructor(private exerciseService: ExerciseService) {
+    super();
+  }
+
+  private checkExerciseSetGroupsValidation(): boolean {
+    return this.exerciseSetGroups && this.exerciseSetGroups.length > 0;
+  }
+
+  private updateValue() {
+    this.onChange(this.exerciseSetGroups); 
+    this.onTouched();
+  }
+
+  private emitValidity(): void {
+    if(this.exerciseSetGroupsValidators && this.exerciseSetGroupsValidators.some(v => v === false)){ 
+      // if at least one exercise set group is invalid
+      this.validityChange.emit(false);
+    }
+    else {
+      this.validityChange.emit(this.isValid);
+      // this.validityChange.emit(this.checkExerciseSetGroupsValidation());
+    }
+  }
+
+  onExerciseSetGroupValidityChange(isValid: boolean, index: number){
+    this.exerciseSetGroupsValidators[index] = isValid;
+    this.emitValidity();
+  }
+
+  writeValue(value: ExerciseSetGroup[]): void {
+    if(value) {
+      this.exerciseSetGroups = value;
+      this.exerciseSetGroupsValidators = Array(this.exerciseSetGroups.length).fill(false); // all exercise set groups are invalid before validation
+      this.isValid = this.checkExerciseSetGroupsValidation();
+    }
+    else {
+      this.exerciseSetGroups = [];
+      this.exerciseSetGroupsValidators = [];
+      this.isValid = false;
+    }
+
+    this.emitValidity();
+  }
+
+  onExerciseSetGroupUpdated(updatedGroup: any, index: number) {
+    this.exerciseSetGroups[index] = updatedGroup;
+    this.updateValue();
+  }
+
+  deleteExerciseSetGroup = async (index: number): Promise<void> => {
+    if (index !== -1) {
+      this.exerciseSetGroups.splice(index, 1);
+      this.exerciseSetGroupsValidators.splice(index, 1);
+
+      if(this.exerciseSetGroups.length === 0) {
+        // if we deleted the last exercise set group
+        this.isValid = false;
+        this.emitValidity();
+      }
+
+      this.updateValue();
+    }
+  };
+
+  deleteAllExerciseSetGroups = async (_: number): Promise<void> => {
+    this.exerciseSetGroups = [];
+    this.exerciseSetGroupsValidators = [];
+
+    this.isValid = false; // there are no exercise set groups
+    this.emitValidity();
+
+    this.updateValue();
+  };
+
+  isAddingNewExercise: boolean = false;
+  addNewExercise(){
+    this.isAddingNewExercise = true;
+  }
+
+  onExerciseIdSelected(exerciseId: number) {
+    (this.exerciseService.getInternalExerciseById(exerciseId) ?? this.exerciseService.getInternalExerciseById(exerciseId))
+      // .pipe(this.catchError())
+      .subscribe((exercise) => {
+        this.exerciseSetGroups.push(this.getDefaultExerciseSetGroupByExercise(exercise));
+        this.exerciseSetGroupsValidators.push(false); // exercise set group is invalid at first
+
+        this.isValid = true; // there is at least one exercise set group
+        this.emitValidity();
+
+        this.isAddingNewExercise = false;
+    })
+  }
+
+  private getDefaultExerciseSetGroupByExercise(exercise: Exercise): ExerciseSetGroup {
+    return <ExerciseSetGroup>{ 
+      id: 0,
+      exerciseId: exercise.id, 
+      exerciseName: exercise.name,
+      exerciseType: exercise.type,
+      exerciseSets: [],
+      sets: 0,
+      weight: <ModelWeight>{},
+    };
+  }
+
+  showCountOfSetsStr(countOfSets: number) {
+    return showCountOfSomethingStr(countOfSets, 'set', 'sets');
+  }
+
+  showCountOfExercisesStr(countOfExercises: number) {
+    return showCountOfSomethingStr(countOfExercises, 'exercise', 'exercises');
+  }
+  
+  expandAll(): void {
+    this.accordion.openAll();
+  }
+
+  collapseAll(): void {
+    this.accordion.closeAll();
+  }
+}
---
+++ WorkoutTracker/src/app/shared/components/editors/exercise-sets-editor/exercise-sets-editor.component.ts
@@ -0,0 +1,159 @@
+import { Component, EventEmitter, Input, Output, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { ExerciseType } from '../../../../exercises/models/exercise-type';
+import { TimeSpan } from '../../../models/time-span';
+import { ModelWeight } from '../../../models/model-weight';
+import { ExerciseSetGroup } from '../../../models/exercise-set-group';
+import { ExerciseSet } from '../../../models/exercise-set';
+import { BaseEditorComponent } from '../../base-editor.component';
+import { WeightType } from 'src/app/shared/models/weight-type';
+
+@Component({
+    selector: 'app-exercise-sets-edit',
+    templateUrl: './exercise-sets-editor.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => ExerciseSetsEditorComponent),
+            multi: true,
+        },
+    ],
+})
+export class ExerciseSetsEditorComponent extends BaseEditorComponent<ExerciseSetGroup> {
+    @Input() weightTypeValue?: WeightType;
+
+    private isValid: boolean = false;
+    @Output() validityChange = new EventEmitter<boolean>();
+  
+    private exerciseSetGroup!: ExerciseSetGroup;
+    
+    exerciseSets!: ExerciseSet[];
+    private exerciseSetsValidators!: boolean[];
+
+    displayedColumns: string[] = ['set', 'value', 'actions'];
+
+    ngOnInit(): void {
+        if(this.value) {
+            this.exerciseSetGroup = this.value;
+
+            if(this.exerciseSetGroup.exerciseSets) {
+                this.exerciseSets = this.exerciseSetGroup.exerciseSets;
+                this.exerciseSetsValidators = Array(this.exerciseSets.length).fill(false); // all exercise sets are invalid before validation
+                this.isValid = this.checkExerciseSetsValidation();
+            }
+            else {
+                this.exerciseSets = []
+                this.exerciseSetsValidators = [];
+                this.isValid = false;
+            }
+            this.isValid = this.checkExerciseSetsValidation();
+        }
+        else {
+            this.exerciseSetGroup = <ExerciseSetGroup>{};
+            this.exerciseSets = []
+            this.exerciseSetsValidators = [];
+            this.isValid = false;
+        }
+        
+        this.emitValidity();
+    }
+ 
+    private checkExerciseSetsValidation(): boolean {
+        return this.exerciseSets && this.exerciseSets.length > 0;
+    }
+
+    private updateValue() {
+      this.emitValidity();
+      this.exerciseSetGroup.exerciseSets = this.exerciseSets;
+      this.onChange(this.exerciseSetGroup); 
+      this.onTouched();
+    }
+
+    private emitValidity(): void {
+        if(this.exerciseSetsValidators && this.exerciseSetsValidators.some(v => v === false)){ 
+            // if at least one exercise set is invalid
+            this.validityChange.emit(false);
+        }
+        else {
+            this.validityChange.emit(this.isValid);
+        }
+    }
+
+    onExerciseSetValidityChange(isValid: boolean, index: number){
+        this.exerciseSetsValidators[index] = isValid;
+        this.emitValidity();
+    }
+
+    writeValue(value: ExerciseSetGroup): void {
+        this.exerciseSetGroup = value;
+    }
+
+    addNewExerciseSet() {
+        var exerciseSet = this.exerciseSets.length === 0 ? this.addFirstExerciseSet() : this.addLastExerciseSet();
+        this.exerciseSets = [...this.exerciseSets, exerciseSet];
+
+        this.exerciseSetsValidators.push(false); // exercise set is invalid at first
+
+        this.isValid = true;
+        this.updateValue();
+    }
+
+    private addFirstExerciseSet(): ExerciseSet {
+        var exerciseSet = <ExerciseSet>{ 
+            exerciseId : this.exerciseSetGroup.exerciseId, 
+            exerciseType : this.exerciseSetGroup.exerciseType, 
+            exerciseName : this.exerciseSetGroup.exerciseName 
+        };
+
+        if(exerciseSet.exerciseType == ExerciseType.Time || exerciseSet.exerciseType == ExerciseType.WeightAndTime)
+            exerciseSet.time = <TimeSpan>{};
+        if(exerciseSet.exerciseType == ExerciseType.WeightAndReps || exerciseSet.exerciseType == ExerciseType.WeightAndTime)
+            exerciseSet.weight = <ModelWeight>{};
+
+        return exerciseSet;
+    }
+
+    private addLastExerciseSet(): ExerciseSet{
+        var exerciseSet = <ExerciseSet>{};
+
+        var lastExerciseSet = this.exerciseSets[this.exerciseSets.length - 1];
+        exerciseSet = {...lastExerciseSet};
+        exerciseSet.weight = <ModelWeight>{...lastExerciseSet.weight};
+        exerciseSet.time = <TimeSpan>{...lastExerciseSet.time};
+        // exerciseSet.id = 0;
+
+        return exerciseSet;
+    }
+
+    deleteExerciseSet = async (index: number): Promise<void> => {
+        if (index !== -1) {
+            this.exerciseSets.splice(index, 1);
+            this.exerciseSets = [...this.exerciseSets];
+            this.exerciseSetsValidators.splice(index, 1);
+
+            if(this.exerciseSets.length === 0) {
+                // if we deleted the last exercise set
+                this.isValid = false;
+                this.emitValidity();
+            }
+
+            this.updateValue();
+        }
+    };
+
+    deleteAllExerciseSets = async (_: number): Promise<void> => {
+        this.exerciseSets = [];
+        this.exerciseSetsValidators = [];
+        
+        this.isValid = false; // there are no exercise sets
+        this.emitValidity();
+
+        this.updateValue();
+    };
+
+    onExerciseSetUpdated(updatedSet: any, index: number) {
+        this.exerciseSets[index] = updatedSet;
+        this.updateValue();
+    }
+}
