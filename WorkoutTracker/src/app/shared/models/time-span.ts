b2d825188562f7133beef2fb5baad00c4669af51
Kurulko <kurul300013@gmail.com>
2025-01-25 09:06:15 +0200

    Updated time-span model

--- WorkoutTracker/src/app/shared/models/time-span.ts
+++ WorkoutTracker/src/app/shared/models/time-span.ts
@@ -1,6 +1,7 @@
-export interface TimeSpan {
-    hours: number;
-    minutes: number;
-    seconds: number;
-    milliseconds: number;
+export class TimeSpan {
+    days: number = 0;
+    hours: number = 0;
+    minutes: number = 0;
+    seconds: number = 0;
+    milliseconds: number = 0;
 }
