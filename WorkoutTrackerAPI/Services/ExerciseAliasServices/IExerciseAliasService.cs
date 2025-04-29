edebab6de51904a52b12714e912490a555548407
Kurulko <kurul300013@gmail.com>
2025-04-29 14:48:21 +0300

    Added exercise alias services

---
+++ WorkoutTrackerAPI/Services/ExerciseAliasServices/IExerciseAliasService.cs
@@ -0,0 +1,17 @@
+﻿using WorkoutTrackerAPI.Data.Models.WorkoutModels;
+using WorkoutTrackerAPI.Data;
+
+namespace WorkoutTrackerAPI.Services.ExerciseAliasServices;
+
+public interface IExerciseAliasService
+{
+    Task<ServiceResult<ExerciseAlias>> GetExerciseAliasByIdAsync(long id);
+    Task<ServiceResult<ExerciseAlias>> GetExerciseAliasByNameAsync(string name);
+
+    Task<ServiceResult<IQueryable<ExerciseAlias>>> GetExerciseAliasesByExerciseIdAsync(long exerciseId);
+
+    Task<ServiceResult<ExerciseAlias>> AddExerciseAliasToExerciseAsync(long exerciseId, ExerciseAlias exerciseAlias);
+    Task<ServiceResult> UpdateExerciseAliasAsync(ExerciseAlias exerciseAlias);
+
+    Task<ServiceResult> DeleteExerciseAliasAsync(long id);
+}
