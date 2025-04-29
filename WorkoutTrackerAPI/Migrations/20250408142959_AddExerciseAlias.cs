2c6d3f42f2500fa42af468c79772e23c82a75e77
Kurulko <kurul300013@gmail.com>
2025-04-29 14:44:47 +0300

    Added migrations

---
+++ WorkoutTrackerAPI/Migrations/20250408142959_AddExerciseAlias.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutTrackerAPI.Migrations
+{
+    public partial class AddExerciseAlias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExerciseAliases",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExerciseId = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExerciseAliases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExerciseAliases_Exercises_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExerciseAliases_ExerciseId",
+                table: "ExerciseAliases",
+                column: "ExerciseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExerciseAliases");
+        }
+    }
+}
