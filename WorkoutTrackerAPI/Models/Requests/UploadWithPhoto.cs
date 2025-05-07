7d685abc78ed99c1f18bec1f79603016afc54ebe
Kurulko <kurul300013@gmail.com>
2025-05-07 10:33:44 +0300

    feat(model): add UploadWithPhoto<T> for handling file uploads

---
+++ WorkoutTrackerAPI/Models/Requests/UploadWithPhoto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkoutTracker.API.Bindings;
+using WorkoutTracker.Application.Common.Models;
+
+namespace WorkoutTracker.API.Models.Requests;
+
+public class UploadWithPhoto<T>
+{
+    public T Model { get; set; } = default!;
+
+    [ModelBinder(BinderType = typeof(FileUploadModelBinder))]
+    public FileUploadModel? Photo { get; set; }
+}
