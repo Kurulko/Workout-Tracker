86f5e0c77c4b392d3a388ff2441faaf1faea8711
Kurulko <kurul300013@gmail.com>
2025-05-30 10:32:25 +0300

    feat: add FileUploadModel validator

---
+++ WorkoutTracker.Infrastructure/Validators/Models/FileUploadModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutTracker.Application.Common.Models;
+using WorkoutTracker.Application.Common.Validators;
+
+namespace WorkoutTracker.Infrastructure.Validators.Models;
+
+public class FileUploadModelValidator
+{
+    public void Validate(FileUploadModel fileUploadModel)
+    {
+        ArgumentValidator.ThrowIfEntryNull(fileUploadModel.Content, nameof(FileUploadModel.Content));
+        ArgumentValidator.ThrowIfArgumentNullOrEmpty(fileUploadModel.FileName, nameof(FileUploadModel.FileName));
+        ArgumentValidator.ThrowIfArgumentNullOrEmpty(fileUploadModel.ContentType, nameof(FileUploadModel.ContentType));
+    }
+}
