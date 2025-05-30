3c4f8788f805c6a224fd00caa68dc172be7dbab6
Kurulko <kurul300013@gmail.com>
2025-05-30 10:31:04 +0300

    feat: add new DTO validators

---
+++ WorkoutTracker.Application/Validators/FileUploadModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using WorkoutTracker.Application.Common.Models;
+using WorkoutTracker.Application.Extensions;
+
+namespace WorkoutTracker.Application.Validators;
+
+internal class FileUploadModelValidator : AbstractValidator<FileUploadModel>
+{
+    public FileUploadModelValidator()
+    {
+        RuleFor(x => x.FileName)
+            .NotEmptyOrNull();
+
+        RuleFor(x => x.ContentType)
+            .NotEmptyOrNull();
+    }
+}
