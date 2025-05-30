6ddd6bf683970a5efc61295d6fe8a9f47d45035f
Kurulko <kurul300013@gmail.com>
2025-05-30 10:29:08 +0300

    feat: add auth model validators

---
+++ WorkoutTracker.Infrastructure/Validators/Models/Auth/AccountModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using WorkoutTracker.Application.Common.Models;
+using WorkoutTracker.Application.Common.Validators;
+using WorkoutTracker.Application.DTOs.Account;
+
+namespace WorkoutTracker.Infrastructure.Validators.Models.Auth;
+
+public abstract class AccountModelValidator<T>
+    where T : AccountModel
+{
+    public virtual void Validate(T model)
+    {
+        ArgumentValidator.ThrowIfArgumentNullOrEmpty(model.Name, nameof(AccountModel.Name));
+        ArgumentValidator.ThrowIfArgumentNullOrEmpty(model.Password, nameof(AccountModel.Password));
+    }
+}
---
+++ WorkoutTracker.Infrastructure/Validators/Models/Auth/LoginModelValidator.cs
@@ -0,0 +1,11 @@
+﻿using WorkoutTracker.Application.DTOs.Account;
+
+namespace WorkoutTracker.Infrastructure.Validators.Models.Auth;
+
+public class LoginModelValidator : AccountModelValidator<LoginModel>
+{
+    public override void Validate(LoginModel model)
+    {
+        base.Validate(model);
+    }
+}
