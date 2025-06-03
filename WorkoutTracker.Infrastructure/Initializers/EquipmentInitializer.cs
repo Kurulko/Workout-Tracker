530c7620cd2b9ead207f4e0938cd7321c4a1b7d1
Kurulko <kurul300013@gmail.com>
2025-06-03 13:44:00 +0300

    refactor: add cancellation token to model initializers

--- WorkoutTracker.Infrastructure/Initializers/EquipmentInitializer.cs
+++ WorkoutTracker.Infrastructure/Initializers/EquipmentInitializer.cs
@@ -5,15 +5,15 @@ namespace WorkoutTracker.Infrastructure.Initializers;
 
 internal class EquipmentInitializer
 {
-    public static async Task<Equipment> InitializeAsync(IEquipmentRepository equipmentRepository, string name)
+    public static async Task<Equipment> InitializeAsync(IEquipmentRepository equipmentRepository, string name, CancellationToken cancellationToken = default)
     {
-        var equipment = await equipmentRepository.GetByNameAsync(name);
+        var equipment = await equipmentRepository.GetByNameAsync(name, cancellationToken);
 
         if (equipment is not null)
             return equipment;
 
         equipment = new Equipment () { Name = name };
-        await equipmentRepository.AddAsync(equipment);
+        await equipmentRepository.AddAsync(equipment, cancellationToken);
         return equipment;
     }
 }
--- WorkoutTracker.Infrastructure/Initializers/WorkoutInitializer.cs
+++ WorkoutTracker.Infrastructure/Initializers/WorkoutInitializer.cs
@@ -10,9 +10,9 @@ namespace WorkoutTracker.Infrastructure.Initializers;
 
 internal static class WorkoutInitializer
 {
-    public static async Task<Workout> InitializeWorkoutAsync(IWorkoutRepository workoutRepository, string workoutName, string userId)
+    public static async Task<Workout> InitializeWorkoutAsync(IWorkoutRepository workoutRepository, string workoutName, string userId, CancellationToken cancellationToken = default)
     {
-        var workout = await workoutRepository.GetByNameAsync(workoutName);
+        var workout = await workoutRepository.GetByNameAsync(workoutName, cancellationToken);
 
         if (workout is not null)
             return workout;
@@ -24,10 +24,10 @@ internal static class WorkoutInitializer
             UserId = userId
         };
 
-        return await workoutRepository.AddAsync(workout);
+        return await workoutRepository.AddAsync(workout, cancellationToken);
     }
 
-    public static async Task<WorkoutRecord> InitializeWorkoutRecordAsync(IWorkoutRecordRepository workoutRecordRepository, string userId, long workoutId, DateTime date, TimeSpan time)
+    public static async Task<WorkoutRecord> InitializeWorkoutRecordAsync(IWorkoutRecordRepository workoutRecordRepository, string userId, long workoutId, DateTime date, TimeSpan time, CancellationToken cancellationToken = default)
     {
         var workoutRecord = new WorkoutRecord()
         {
@@ -37,10 +37,10 @@ internal static class WorkoutInitializer
             UserId = userId
         };
 
-        return await workoutRecordRepository.AddAsync(workoutRecord);
+        return await workoutRecordRepository.AddAsync(workoutRecord, cancellationToken);
     }
 
-    public static async Task<ExerciseRecordGroup> InitializeExerciseRecordGroupAsync(IExerciseRecordGroupRepository exerciseRecordGroupRepository, long exerciseId, long workoutRecordId)
+    public static async Task<ExerciseRecordGroup> InitializeExerciseRecordGroupAsync(IExerciseRecordGroupRepository exerciseRecordGroupRepository, long exerciseId, long workoutRecordId, CancellationToken cancellationToken = default)
     {
         var exerciseRecordGroup = new ExerciseRecordGroup()
         {
@@ -48,10 +48,10 @@ internal static class WorkoutInitializer
             WorkoutRecordId = workoutRecordId,
         };
 
-        return await exerciseRecordGroupRepository.AddAsync(exerciseRecordGroup);
+        return await exerciseRecordGroupRepository.AddAsync(exerciseRecordGroup, cancellationToken);
     }
 
-    public static async Task InitializeExerciseRecordAsync(IExerciseRecordRepository exerciseRecordRepository, DateTime date, long exerciseId, long exerciseRecordGroupId, string userId, int sets = 1, int? reps = null, ModelWeight? weight = null, TimeSpan? time = null)
+    public static async Task InitializeExerciseRecordAsync(IExerciseRecordRepository exerciseRecordRepository, DateTime date, long exerciseId, long exerciseRecordGroupId, string userId, int sets = 1, int? reps = null, ModelWeight? weight = null, TimeSpan? time = null, CancellationToken cancellationToken = default)
     {
         for (int i = 0; i < sets; i++)
         {
@@ -65,11 +65,11 @@ internal static class WorkoutInitializer
                 Reps = reps
             };
 
-            await exerciseRecordRepository.AddAsync(exerciseRecord);
+            await exerciseRecordRepository.AddAsync(exerciseRecord, cancellationToken);
         }
     }
 
-    public static async Task<ExerciseSetGroup> InitializeExerciseSetGroupAsync(IExerciseSetGroupRepository exerciseSetGroupRepository, long exerciseId, long workoutId)
+    public static async Task<ExerciseSetGroup> InitializeExerciseSetGroupAsync(IExerciseSetGroupRepository exerciseSetGroupRepository, long exerciseId, long workoutId, CancellationToken cancellationToken = default)
     {
         var exerciseSetGroup = new ExerciseSetGroup()
         {
@@ -77,10 +77,10 @@ internal static class WorkoutInitializer
             WorkoutId = workoutId,
         };
 
-        return await exerciseSetGroupRepository.AddAsync(exerciseSetGroup);
+        return await exerciseSetGroupRepository.AddAsync(exerciseSetGroup, cancellationToken);
     }
 
-    public static async Task InitializeExerciseSetAsync(IExerciseSetRepository exerciseSetRepository, long exerciseId, long exerciseSetGroupId, string userId, int sets = 1, int? reps = null, ModelWeight? weight = null, TimeSpan? time = null)
+    public static async Task InitializeExerciseSetAsync(IExerciseSetRepository exerciseSetRepository, long exerciseId, long exerciseSetGroupId, string userId, int sets = 1, int? reps = null, ModelWeight? weight = null, TimeSpan? time = null, CancellationToken cancellationToken = default)
     {
         for (int i = 0; i < sets; i++)
         {
@@ -93,7 +93,7 @@ internal static class WorkoutInitializer
                 Reps = reps
             };
 
-            await exerciseSetRepository.AddAsync(exerciseSet);
+            await exerciseSetRepository.AddAsync(exerciseSet, cancellationToken);
         }
     }
 }
