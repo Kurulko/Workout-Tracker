8d182ca787dc1c553b9e239d5f7cb825a33d6360
Kurulko <kurul300013@gmail.com>
2025-06-03 13:44:24 +0300

    feat: add exercise aliases to ExercisesInitializer

--- WorkoutTracker.Infrastructure/Initializers/ExercisesInitializer.cs
+++ WorkoutTracker.Infrastructure/Initializers/ExercisesInitializer.cs
@@ -11,11 +11,22 @@ namespace WorkoutTracker.Infrastructure.Initializers;
 
 internal class ExercisesInitializer
 {
-    public static async Task<Exercise> InitializeAsync(IExerciseRepository exerciseRepository, IMuscleRepository muscleRepository, IEquipmentRepository equipmentRepository, string name, string? image, string[] equipmentNames, ExerciseType exerciseType, string[] muscleNames)
+    public static async Task<Exercise> InitializeAsync(
+        IExerciseRepository exerciseRepository, 
+        IMuscleRepository muscleRepository, 
+        IEquipmentRepository equipmentRepository, 
+        IExerciseAliasRepository exerciseAliasRepository, 
+        string name, 
+        string? image, 
+        string[] equipmentNames, 
+        ExerciseType exerciseType, 
+        string[] muscleNames, 
+        string[]? aliasesStr = null, 
+        CancellationToken cancellationToken = default)
     {
-        var exercise = await exerciseRepository.GetByNameAsync(name);
+        var exercise = await exerciseRepository.GetByNameAsync(name, cancellationToken);
 
-        if (exercise is not null)
+        if (exercise != null)
             return exercise;
 
         exercise = new ()
@@ -25,36 +36,47 @@ internal class ExercisesInitializer
             Type = exerciseType
         };
 
-        exercise.WorkingMuscles = await GetMuscles(muscleRepository, muscleNames);
-        exercise.Equipments = await GetEquipments(equipmentRepository, equipmentNames);
+        exercise.WorkingMuscles = await GetMusclesAsync(muscleRepository, muscleNames, cancellationToken);
+        exercise.Equipments = await GetEquipmentsAsync(equipmentRepository, equipmentNames, cancellationToken);
 
-        await exerciseRepository.AddAsync(exercise);
+        await exerciseRepository.AddAsync(exercise, cancellationToken);
+
+        if (aliasesStr != null && aliasesStr.Any())
+            await AddExerciseAliasesAsync(exerciseAliasRepository, exercise.Id, aliasesStr, cancellationToken);
+
         return exercise;
     }
 
-    static async Task<ICollection<Muscle>> GetMuscles(IMuscleRepository muscleRepository, string[] muscleNames)
+    static async Task<ICollection<Muscle>> GetMusclesAsync(IMuscleRepository muscleRepository, string[] muscleNames, CancellationToken cancellationToken)
     {
         var muscles = new List<Muscle>();
 
         foreach (string muscleName in muscleNames)
         {
-            var muscle = await ArgumentValidator.EnsureExistsByNameAsync(muscleRepository.GetByNameAsync, muscleName, nameof(Muscle));
+            var muscle = await ArgumentValidator.EnsureExistsByNameAsync(muscleRepository.GetByNameAsync, muscleName, nameof(Muscle), cancellationToken);
             muscles.Add(muscle);
         }
 
         return muscles;
     }
 
-    static async Task<ICollection<Equipment>> GetEquipments(IEquipmentRepository equipmentRepository, string[] equipmentNames)
+    static async Task<ICollection<Equipment>> GetEquipmentsAsync(IEquipmentRepository equipmentRepository, string[] equipmentNames, CancellationToken cancellationToken)
     {
         var equipments = new List<Equipment>();
 
         foreach (string equipmentName in equipmentNames)
         {
-            var equipment = await ArgumentValidator.EnsureExistsByNameAsync(equipmentRepository.GetByNameAsync, equipmentName, nameof(Equipment));
+            var equipment = await ArgumentValidator.EnsureExistsByNameAsync(equipmentRepository.GetByNameAsync, equipmentName, nameof(Equipment), cancellationToken);
             equipments.Add(equipment);
         }
 
         return equipments;
+    }
+
+    static async Task AddExerciseAliasesAsync(IExerciseAliasRepository exerciseAliasRepository, long exerciseId, string[] aliasesStr, CancellationToken cancellationToken)
+    {
+        var aliases = aliasesStr.Select(alias => new ExerciseAlias() { Name = alias, ExerciseId = exerciseId });
+
+        await exerciseAliasRepository.AddRangeAsync(aliases, cancellationToken);
     }
 }
